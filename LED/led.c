51ba2dc77c225343f97cfc31695d9d4c16a9a113
Islam Tarek <islamtarek0550@gmail.com>
2022-07-13 17:57:51 +0200

    implementation of led_set_staate API

--- LED/led.c
+++ LED/led.c
@@ -59,9 +59,22 @@ hal_err_t led_init(led_id_t id)
 
 }
 
-hal_err_t led_set_state (led_id_t id, led_state_t state)
+hal_err_t led_set_state(led_id_t id, led_state_t state)
 {
-
+    if(id < LED_MAX)
+    {
+        if(led_control(id,state) == HAL_OK)
+        {
+            LEDS[id].led_state = state;
+            return HAL_OK;
+        }
+        else
+            return HAL_FAIL;
+    }
+    else
+    {
+        return HAL_FAIL;
+    }
 }
 
 hal_err_t led_get_state (led_id_t id, led_state_t* state);
