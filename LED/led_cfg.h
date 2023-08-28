b3d5e762aaf9ff1d54544d0dca2ee7141c549d1f
Islam Tarek <islam.tarek@valeo.com>
2023-08-28 15:44:02 +0300

    Add Number of LEDs Macro

--- LED/led_cfg.h
+++ LED/led_cfg.h
@@ -2,11 +2,22 @@
  * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
  * @CreatedDate           : 2023-08-28 15:06:02                              *
  * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
- * @LastEditDate          : 2023-08-28 15:06:03                              *
+ * @LastEditDate          : 2023-08-28 15:41:38                              *
  * @FilePath              : led_cfg.h                                        *
  ****************************************************************************/
 
 #ifndef LED_LED_CFG_H_
 #define LED_LED_CFG_H_
 
+/**
+ * @section Definitions
+ */
+
+/**
+ * @brief Number of LEDs
+ * @note This macro contains the number of LEDs that will be used in the project.
+ */
+
+#define NUMBER_OF_LEDS      
+    
 #endif
