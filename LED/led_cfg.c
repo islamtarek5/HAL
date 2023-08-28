a2f05ec2e3c0b0f5950c54de7bca1e062ba16993
Islam Tarek <islam.tarek@valeo.com>
2023-08-28 15:48:41 +0300

    Add LED configuraton array

--- LED/led_cfg.c
+++ LED/led_cfg.c
@@ -2,6 +2,27 @@
  * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
  * @CreatedDate           : 2023-08-28 15:06:10                              *
  * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
- * @LastEditDate          : 2023-08-28 15:06:51                              *
+ * @LastEditDate          : 2023-08-28 15:47:49                              *
  * @FilePath              : led_cfg.c                                        *
  ****************************************************************************/
+
+/**
+ * @section Includes
+ */
+
+#include "led.h"
+#include "led_cfg.h"
+
+
+/**
+ * @section Global Variables
+ */
+
+/**
+ * @brief LED Configuration Array
+ */
+
+Led_S LEDs_CFG[NUMBER_OF_LEDS] =
+{
+
+};
