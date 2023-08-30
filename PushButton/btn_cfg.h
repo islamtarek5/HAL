7efa386cc9d97d997f7e705d0a5b5eb70e30c7bb
Islam Tarek <islam.tarek@valeo.com>
2023-08-30 16:11:39 +0300

    Add Btn APIs and Types

--- PushButton/btn_cfg.h
+++ PushButton/btn_cfg.h
@@ -2,11 +2,26 @@
  * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
  * @CreatedDate           : 2023-08-30 14:32:04                              *
  * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
- * @LastEditDate          : 2023-08-30 14:32:49                              *
+ * @LastEditDate          : 2023-08-30 16:08:19                              *
  * @FilePath              : btn_cfg.h                                        *
  ****************************************************************************/
 
 #ifndef BTN_BTN_CFG_H_
 #define BTN_BTN_CFG_H_
+
+/**
+ * @section typedefs
+ */
+
+/**
+ * @brief BTN ID
+ * @note This Enum will contain the BTNs' IDs in project.
+ * !note BTN_Max_ID must not deleted as it's used in code.
+ */
+typedef enum
+{
+    // TODO: Add BTNs' IDs
+    BTN_MAX_ID
+}btn_id_t;
 
 #endif
